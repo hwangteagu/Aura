f4b1d3f06c0a9fca8ccc8847e305d073acf0e8a4
Hwang.T.G <hwangteagu@gmail.com>
2026-02-28 14:18:06 +0900

    The Character and Enemy Class

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Copyright TG STudio
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Character/AuraEnemyCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright TG STudio
+
+
+#include "Character/AuraEnemyCharacter.h"
+
